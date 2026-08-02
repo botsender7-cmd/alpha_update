a9798fcd0664c23393fccc16aac2347fc3e4c48e
botsender7-cmd <botsender7@gmail.com>
2026-08-02 16:44:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -418,5 +418,5 @@ def check():
 
 if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
-    port  = int(os.environ.get("PORT", "5001"))
+    port  = int(os.environ.get("PORT", "5000"))
     app.run(debug=debug, host="0.0.0.0", port=port, threaded=True)
